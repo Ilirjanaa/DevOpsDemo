8d1d1728e8f26af22d7ea5f8add99900c1ecc5f8
Ilirjanaa <berisil1@students.zhaw.ch>
2023-06-13 10:50:30 +0200

    Test2

---
+++ backend/src/main/java/ch/zhaw/iwi/devops/TicTacToeGame/TicTacToeGame.java
@@ -0,0 +1,80 @@
+package ch.zhaw.iwi.devops.TicTacToeGame;
+
+public class TicTacToeGame {
+    
+    private char[][] board;
+    private char currentPlayer;
+
+    public TicTacToeGame() {
+        board = new char[3][3];
+        currentPlayer = 'X';
+        initializeBoard();
+    }
+
+    public void play(int row, int col) {
+        if (isFieldEmpty(row, col)) {
+            board[row][col] = currentPlayer;
+            currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+        }
+    }
+
+    public boolean isGameOver() {
+        return isBoardFull() || hasPlayerWon();
+    }
+
+    private void initializeBoard() {
+        for (char[] row : board) {
+            for (int i = 0; i < row.length; i++) {
+                row[i] = ' ';
+            }
+        }
+    }
+
+    private boolean isFieldEmpty(int row, int col) {
+        return board[row][col] == ' ';
+    }
+
+    private boolean isBoardFull() {
+        for (char[] row : board) {
+            for (char field : row) {
+                if (field == ' ') {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private boolean hasPlayerWon() {
+        for (int i = 0; i < 3; i++) {
+            // Überprüfe Reihen
+            if (board[i][0] == currentPlayer && board[i][1] == currentPlayer && board[i][2] == currentPlayer) {
+                return true;
+            }
+            
+            // Überprüfe Spalten
+            if (board[0][i] == currentPlayer && board[1][i] == currentPlayer && board[2][i] == currentPlayer) {
+                return true;
+            }
+        }
+    
+        // Überprüfe Diagonalen
+        if (board[0][0] == currentPlayer && board[1][1] == currentPlayer && board[2][2] == currentPlayer) {
+            return true;
+        }
+    
+        if (board[0][2] == currentPlayer && board[1][1] == currentPlayer && board[2][0] == currentPlayer) {
+            return true;
+        }
+    
+        return false;
+    }
+
+    public char getFieldValue(int row, int col) {
+        if (isFieldEmpty(row, col)) {
+            return ' ';
+        } else {
+            return board[row][col];
+        }
+    }
+}
---
+++ backend/src/test/java/ch/zhaw/iwi/devops/TicTacToeGame/TicTacToeTest.java
@@ -0,0 +1,29 @@
+package ch.zhaw.iwi.devops.TicTacToeGame;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class TicTacToeTest {
+    
+    private TicTacToeGame game;
+
+    @BeforeEach
+    public void setUp() {
+        game = new TicTacToeGame();
+    }
+
+    @Test
+    public void Test1() {
+        game.play(0, 0);
+        Assertions.assertEquals('X', game.getFieldValue(0, 0));
+    }
+
+    @Test
+    public void Test2() {
+        game.play(0, 0);
+        Assertions.assertEquals('1', game.getFieldValue(0, 0));
+    }
+
+    
+}   
