2fabc6c1e08cd141bd45020445a9b15bdc4c604a
Ilirjanaa <berisil1@students.zhaw.ch>
2023-06-17 12:03:24 +0200

    Testerweiterung

--- demo/src/test/java/ch/zhaw/springboot/demo/DemoApplicationTests.java
+++ demo/src/test/java/ch/zhaw/springboot/demo/DemoApplicationTests.java
@@ -8,6 +8,11 @@ class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
+
+	assertTrue(true);
+
+	int resut = yoourService.add(5, 10)
+	assertEquals(15, result, "Die Addition sollte das erwartete Ergebnis zurückgeben.");
 	}
 
 }
